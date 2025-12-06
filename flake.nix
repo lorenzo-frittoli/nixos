ecd93ea7c22af63f72fcbd006e83b28faaec7f03
Lorenzo Frittoli <lorenzo.frittoli.ge@gmail.com>
2025-12-06 23:26:41 +0100

    boh

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   # REMEMBER TO UPDATE ALL VERSIONS
   # current version = 25.05;
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     disko = {
       url = "github:nix-community/disko";
@@ -12,12 +12,12 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     stylix = {
-      url = "github:danth/stylix/release-25.05";
+      url = "github:danth/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
