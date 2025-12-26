9f95ffd01ec8baedb72dc2fd759d7f2e44795450
Lorenzo Frittoli <lorenzo.frittoli.ge@gmail.com>
2025-12-26 21:49:26 +0100

    Update boot.nix because it brokey

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,5 +1,15 @@
 {
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader = {
+    # If you want to use GRUB
+    grub = {
+      enable = true;
+      device = "nodev"; # Required for EFI
+      efiSupport = true;
+      # enableCryptodisk = true; # Uncomment if using LUKS on root
+    };
+    
+    # You also need to point to the EFI partition
+    # Disko usually mounts this at /boot, so this is the default
+    efi.canTouchEfiVariables = true;
+  };
 }
